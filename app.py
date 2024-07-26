42a3ba99cfcac678612c86a1fcbc6d9cb84ddf9f
OseiFrancis1 <osei.francis090@gmail.com>
2024-07-26 11:27:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,36 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Title of the app
+st.title("Streamlit st.write() Demonstration")
 
-#set the title of the app
-st.title("Welcome to My First Stream lit app")
-
-#Add a text input
-name= st.text_input("Enter your name:")
-
-#display the name entered by the user
-if name:
-  st.write(f"Hello, {name}! Welcome to the app.")
-
+# Writing text
+st.write("## This is a header")
+st.write("This is a simple text example using `st.write()`.")
 
 # Writing dataframes
 data = {
     "Column 1": [1, 2, 3, 4],
     "Column 2": [10, 20, 30, 40]
 }
+df = pd.DataFrame(data)
+st.write("### Here is a dataframe")
+st.write(df)
 
+# Writing charts
+st.write("### Here is a chart")
+fig, ax = plt.subplots()
+ax.plot(df["Column 1"], df["Column 2"])
+st.write(fig)
+
+# Writing markdown
+st.write("### This is markdown")
+st.write("""
+* Item 1
+* Item 2
+* Item 3
+""")
+
+# Writing a dictionary
+st.write("### Here is a dictionary")
+st.write({"name": "Tommy", "age": 10})
