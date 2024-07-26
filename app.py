af0b6c55f183c8fa541304fb50a53654e646b497
OseiFrancis1 <osei.francis090@gmail.com>
2024-07-26 11:00:23 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+#set the title of the app
+st.title("Welcome to My First Stream lit app")
+
+#Add a text input
+name= st.text_input("Enter your name:")
+
+#display the name entered by the user
+if name:
+  st.write(f"Hello, {name}! Welcome to the app.")
